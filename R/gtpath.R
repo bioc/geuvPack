07e9a0e5623c37745b8a6b3c7a763bc465f3323a
Vince Carey <stvjc@channing.harvard.edu>
2015-09-13 21:20:58 +0000

    minor change to handle x chrom
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/geuvPack@3403 db2202da-8704-0410-b924-b125b3cd84ef

--- R/gtpath.R
+++ R/gtpath.R
@@ -1,16 +1,35 @@
+#
+#
+#gtpath = function (chrdigit, useS3 = TRUE, tmplate)
+#{
+#    if (is.numeric(chrdigit)) chrdigit=as.integer(chrdigit)
+#    if (is(chrdigit, "integer")) chrdigit=paste0("chr", chrdigit)
+#    if (length(grep("chr", chrdigit))<1) warning("probably need 'chr' in input string")
+#    if (useS3) {
+#        tmplate = "http://1000genomes.s3.amazonaws.com/release/20130502/ALL.%%N%%.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz"
+#        if (length(grep("X|Y", chrdigit))>0) 
+#            tmplate = "http://1000genomes.s3.amazonaws.com/release/20130502/ALL.%%N%%.phase3_integrated_v1b.20130502.genotypes.vcf.gz"
+#        }
+#    else if (missing(tmplate)) stop("must supply tmplate with %%N%% for substitution if useS3 is false.")
+#    gsub("%%N%%", chrdigit, tmplate)
+#}
 
-
-gtpath = function (chrdigit, useS3 = TRUE, tmplate)
-{
-    if (is.numeric(chrdigit)) chrdigit=as.integer(chrdigit)
-    if (is(chrdigit, "integer")) chrdigit=paste0("chr", chrdigit)
-    if (length(grep("chr", chrdigit))<1) warning("probably need 'chr' in input string")
+gtpath = function (chrdigit, useS3 = TRUE, tmplate)  {
+    if (is.numeric(chrdigit)) 
+        chrdigit = as.integer(chrdigit)
+    if (is(chrdigit, "integer")) 
+        chrdigit = paste0("chr", chrdigit)
+    if (length(grep("chr", chrdigit)) < 1) 
+        warning("probably need 'chr' in input string")
     if (useS3) {
         tmplate = "http://1000genomes.s3.amazonaws.com/release/20130502/ALL.%%N%%.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz"
-        if (length(grep("X|Y", chrdigit))>0) 
+        if (length(grep("X", chrdigit)) > 0) 
+            tmplate = "http://1000genomes.s3.amazonaws.com/release/20130502/ALL.%%N%%.phase3_shapeit2_mvncall_integrated_v1b.20130502.genotypes.vcf.gz"
+        if (length(grep("Y", chrdigit)) > 0) 
             tmplate = "http://1000genomes.s3.amazonaws.com/release/20130502/ALL.%%N%%.phase3_integrated_v1b.20130502.genotypes.vcf.gz"
-        }
-    else if (missing(tmplate)) stop("must supply tmplate with %%N%% for substitution if useS3 is false.")
+    }
+    else if (missing(tmplate)) 
+        stop("must supply tmplate with %%N%% for substitution if useS3 is false.")
     gsub("%%N%%", chrdigit, tmplate)
 }
 
